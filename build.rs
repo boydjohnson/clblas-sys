9920261b083e108c8a9bd2687eb3946365b2f532
Boyd Johnson <johnson.boyd@gmail.com>
2020-11-27 22:29:55 +0000

    Remove some opencl types

--- build.rs
+++ build.rs
@@ -4,13 +4,15 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    
     println!("cargo:rustc-link-lib=clBLAS");
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .blacklist_function("^cl[A-Z].*")
+        .blacklist_item("pthread.*")
+        .blacklist_item("^CL_.*")
         .generate()
         .expect("Unable to generate bindings");
 
